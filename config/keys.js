ae6b2427f2736f4db31386efc941f65882ee5371
Rajkumar Pal <bornagain.raj7@gmail.com>
2019-04-09 00:03:51 +0530

    minor changes

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientId: '718782623721-3bshdhkpstjjvcdkuqne3106ijl8m9nb.apps.googleusercontent.com',
-        clientSecret: 'XnzkYn4w3T4kImKjlAPmEC2I'
-    }
-}
